83c4103236368e9731b1154befc62a15eb4cac8f
Tejas Vasisht <53622454+tejasvasisht@users.noreply.github.com>
2022-03-23 12:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 st.title("SPAM HAM CLASSIFIER")
 text_model=joblib.load('spam-ham')
-ip=st.text_input("Enter your message: ")
+ip=st.text_input("Ente r your message: ")
 op=text_model.predict([ip])
 if st.button('Predict'):
   st.title(op[0])
